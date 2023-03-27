6218ca9d84d5bd9e739a25729bd59ef52a51e1b5
SSID08 <112456108+SSID08@users.noreply.github.com>
2023-03-28 04:12:43 +0530

    Update fabricNetwork.js

--- fabricNetwork.js
+++ fabricNetwork.js
@@ -3,7 +3,7 @@ const { Gateway, Wallets } = require('fabric-network');
 //const FabricCAServices = require('fabric-ca-client');
 const path = require('path');
 //const { buildCAClient, registerAndEnrollUser, enrollAdmin } = require('../test-application/javascript/CAUtil.js');
-const { buildCCPOrg1, buildWallet } = require('../../test-application/javascript/AppUtil.js');
+const { buildCCPOrg1, buildWallet } = require('./test-application/javascript/AppUtil.js');
 
 const channelName = 'mychannel';
 const chaincodeName = 'basic';
